62f8a97b4837aa205e1ab598cd5ace4c0889d5c9
Param Singh <paramsingh258@gmail.com>
2017-04-15 01:12:32 +0400

    Work on the donate and pay methods

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flask import request, render_template, send_file, request
-import config
-from bigchaindb_driver import BigchainDB
-from bigchaindb_driver.crypto import generate_keypair
-
+from utils import bdb_donate
 
 app = Flask(__name__)
 blockchain_db = BigchainDB(config.BLOCKCHAIN_URL)
+user = generate_keypair()
 
 @app.route('/')
 def index():
@@ -17,14 +15,27 @@ def donate():
     """
     Take a donater name and an amount and put a transaction into the blockchain
     """
-    pass
+    if request.method == 'GET':
+        return render_template('donate.html')
+    elif request.method == 'POST':
+        donater_name = request.form.get('donater_name')
+        amount = request.form.get('amount')
+        sent_txid = bdb_donate(blockchain_db, user, donater_name, amount)
+        return "Transaction {} sent".format(sent_txid)
 
-@app.route('/pay', methods=['POST'])
+
+@app.route('/pay', methods=['GET', 'POST'])
 def pay():
     """
     Take a vendor name and an amount and put that expenditure transaction into the blockchain
     """
-    pass
+    if request.method == 'GET':
+        return render_template('pay.html')
+    elif request.method == 'POST':
+        vendor_name = request.form.get('vendor_name')
+        amount = request.form.get('amount')
+        sent_txid = bdb_pay(blockchain_db, user, vendor_name, amount)
+        return "Transaction {} sent".format(sent_txid)
 
 
 if __name__ == '__main__':
